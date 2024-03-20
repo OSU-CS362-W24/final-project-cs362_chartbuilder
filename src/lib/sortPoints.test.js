c6e9d357f6a2bfb175c606cb795f2bfbee466af4
Justin Isert <isertj@oregonstate.edu>
2024-03-19 21:18:04 -0700

    Added test checking sortPoints() returns a sorted array

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -14,3 +14,23 @@ test("sortPoints() returns a reference to the original array passed into it", fu
    expect(resultData).toBe(testData)
 })
 
+test("sortPoints() returns a reference to the original array passed into it", function(){
+   // Arrange
+   testData = [
+      { x: 3, y: 6},
+      { x: 5, y: 10},
+      { x: 8, y: 8},
+      { x: 2, y: 1},
+      { x: 1, y: 2}
+   ]
+
+   // Act 
+   resultData = sortPoints(testData)
+
+   // Assert
+   expect(resultData[1].x).toBeGreaterThan(resultData[0].x)
+   expect(resultData[2].x).toBeGreaterThan(resultData[1].x)
+   expect(resultData[3].x).toBeGreaterThan(resultData[2].x)
+   expect(resultData[4].x).toBeGreaterThan(resultData[3].x)
+})
+
