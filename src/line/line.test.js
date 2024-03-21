4246e4bcdd7b52a8be3488feea55a76349b0d819
Justin Isert <isertj@oregonstate.edu>
2024-03-20 19:07:14 -0700

    Added more comments

--- src/line/line.test.js
+++ src/line/line.test.js
@@ -21,17 +21,21 @@ test('clicking the "add values" button adds 2 input boxes when clicked', async f
    // Arrange:
    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
 
+   // Finds the '+' button 
    const addValButton = domTesting.getByRole(document, "button", { name: "+" })
 
+   // Checks that there are only 2 number input fields before button is pressed
    const valueInputsBefore = domTesting.queryAllByRole(document, 'spinbutton')
    expect(valueInputsBefore).toHaveLength(2)
 
    // Act: 
    const user = userEvent.setup()
+   // Click the '+' button once, adding two number input field
    await user.click(addValButton)
 
    // Assert:
    const valueInputsAfter = domTesting.queryAllByRole(document, 'spinbutton')
+   // Asserts that there should be four number input fields
    expect(valueInputsAfter).toHaveLength(4)
 })
 
@@ -39,16 +43,19 @@ test('clicking the "add values" button adds input boxes for multiple button clic
    // Arrange:
    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
 
+   // Finds the '+' button 
    const addValButton = domTesting.getByRole(document, "button", { name: "+" })
 
    // Act: 
    const user = userEvent.setup()
+   // User clicks the '+' button four times, adding eight number input fields
    await user.click(addValButton)
    await user.click(addValButton)
    await user.click(addValButton)
    await user.click(addValButton)
 
    // Assert:
+   // Asserts that there should be ten number input fields
    const valueInputsAfter = domTesting.queryAllByRole(document, 'spinbutton')
    expect(valueInputsAfter).toHaveLength(10)
 })
@@ -57,19 +64,25 @@ test('clicking the "add values" button does not impact data the user input', asy
    // Arrange:
    initDomFromFiles(`${__dirname}/line.html`, `${__dirname}/line.js`)
 
+   // Finds the '+' button 
    const addValButton = domTesting.getByRole(document, "button", { name: "+" })
 
    // Act: 
    const user = userEvent.setup()
+   // User clicks the '+' button once, adding two number input fields
    await user.click(addValButton)
 
+   // Finds all number inputs
    const valueInputs = domTesting.queryAllByRole(document, 'spinbutton')
+   // Types '1' into the first number input 
    await user.type(valueInputs[0], '1')
+   // Types '1' into the second number input 
    await user.type(valueInputs[1], '2')
 
    await user.click(addValButton)
 
    // Assert:
+   // Asserts that the values entered by the user were not impacted
    const valueInputsAfter = domTesting.queryAllByRole(document, 'spinbutton')
    expect(valueInputsAfter[0]).toHaveValue(1)
    expect(valueInputsAfter[1]).toHaveValue(2)
