94db483bbde32d0a2dbe6ed9b442bcda29370ede
isertj <72046550+isertj@users.noreply.github.com>
2024-03-21 05:42:53 +0000

    Added test for saving a chart to gallery

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -67,4 +67,44 @@ describe('template spec', () => {
     cy.findByLabelText('X label').should('have.value', 'Cats');
     cy.findByLabelText('Y label').should('have.value', 'Dogs');
   })
+
+  it('Check that a chart can be saved to the gallery', () => {
+      //If you're seeing this as a block of code and not as a function then please pretend. I'll fix it later :)
+      cy.visit('/') //Should be changed to a generic variable later
+      cy.findByText("Scatter").click()
+      cy.findByLabelText('Chart title').type('Age vs. Height');
+      cy.findByLabelText('X label').type('Age');
+      cy.findByLabelText('Y label').type('Height');
+
+      //create buttons with + button and populate them with data as per video
+      for (let i = 0; i < 4; i++) {
+      cy.findByRole("button", {name: "+"}).click();
+      }
+    
+      //Populate variables section of graph with numbers. Done in a very icky way because they're auto-generated buttons w    ith no discernable label
+      //Would absolutely go better in a loop but I couldn't find a pattern in the y-value nums
+      cy.get(`:nth-child(${4}) > .x-value-input`).type(1);
+      cy.get(`:nth-child(${5}) > .y-value-input`).type(3);
+      cy.get(`:nth-child(${6}) > .x-value-input`).type(2);
+      cy.get(`:nth-child(${7}) > .y-value-input`).type(7);
+      cy.get(`:nth-child(${8}) > .x-value-input`).type(3);
+      cy.get(`:nth-child(${9}) > .y-value-input`).type(15);
+      cy.get(`:nth-child(${10}) > .x-value-input`).type(4);
+      cy.get(`:nth-child(${11}) > .y-value-input`).type(25);
+      cy.get(`:nth-child(${12}) > .x-value-input`).type(5);
+      cy.get(`:nth-child(${13}) > .y-value-input`).type(40);
+    
+      // Generates and saves the chart
+      cy.findByRole("button", {name: "Generate chart"}).click();
+      cy.findByRole("button", {name: "Save chart"}).click();
+
+      // Navigates to the Gallery page
+      cy.findByRole("link", {name: "Gallery"}).click();
+
+      // Checks that the chart image exists on the Gallery page
+      cy.get('img', {name: 'chart-img'}).should('exist'); //Website won't build images if there isn't data present
+      cy.findByLabelText('Chart title').should('have.value', 'Age vs. Height'); //These check the persistence of labels. I d    id not test every single x and y value because that would look messy and I don't feel like it would accomplish more
+      cy.findByLabelText('X label').should('have.value', 'Age');
+      cy.findByLabelText('Y label').should('have.value', 'Height');
+      })
 })
